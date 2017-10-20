49f98525a78b748c5b39ab3b63d12040313c5b09
GitCodeAllen <CodeAllen@qq.com>
2017-10-20 13:10:12 +0800

    Prepared for Multithread

--- ProjectSummary.py
+++ ProjectSummary.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import time
+import threading
 
 #需要统计行数的文件后缀
 Suffix = {".c",".cpp",".h",".hpp",".cxx"}
@@ -34,18 +36,20 @@ def CountFileLines(FileName):
         count+=1
     return count
 
-def listdir(dir,Targets):
+def listdir(dir,Targets,isSearchFiles = False):
     files = os.listdir(dir)
     for file in files:
         filepath = os.path.join(dir,file)
         if os.path.isdir(filepath):
-            listdir(filepath,Targets)
+            listdir(filepath,Targets,isSearchFiles)
         elif os.path:
             suffix = os.path.splitext(file)[1]
             if Targets.has_key(suffix):
                 if not Targets[suffix].has_key("TotalFiles"):
                     Targets[suffix]["TotalFiles"] = 0
-                Targets[suffix]["TotalFiles"]+=1
+                Targets[suffix]["TotalFiles"] += 1
+                if isSearchFiles:
+                    continue
                 lines = CountFileLines(filepath)
                 if not Targets[suffix].has_key("TotalLines"):
                     Targets[suffix]["TotalLines"] = 0
@@ -55,26 +59,52 @@ def listdir(dir,Targets):
                 if Targets[suffix]["TheMaxLines"] < lines:
                     Targets[suffix]["TheMaxLines"] = lines
 
-Targets = {}
-files = 0;
-lines = 0;
-for val in Suffix:
-    Targets[val] = {}
-                    
-listdir(Path,Targets)
+def Count(Targets):
+    files = 0
+    lines = 0
+    for suffix_val in Targets.keys():
+        if not Targets[suffix_val]:
+            continue
+        for suffix_item in Targets[suffix_val].keys():
+            if suffix_item == "TotalFiles":
+                files += Targets[suffix_val][suffix_item]
+            if suffix_item == "TotalLines":
+                lines += Targets[suffix_val][suffix_item]
+    return (files,lines)
 
-for suffix_val in Targets.keys():
-    if not Targets[suffix_val]:
-        continue
-    print(suffix_val+":")
-    for suffix_item in Targets[suffix_val].keys():
-        if suffix_item == "TotalFiles":
-            files += Targets[suffix_val][suffix_item]
-        if suffix_item == "TotalLines":
-            lines += Targets[suffix_val][suffix_item]
-        print("\t"+suffix_item+"\t:"+"{:,}".format(Targets[suffix_val][suffix_item]))
-    print('\n')
-    
-print("Total:")
-print("\tFiles\t:"+"{:,}".format(files))
-print("\tLines\t:"+"{:,}".format(lines))
+def DynamicShow(ShowStr):
+    sys.stdout.write(('{:<20}').format(ShowStr)+'\r')
+    sys.stdout.flush()
+
+def EndDynamicShow():
+    print('')
+
+def ShowResults(Targets):
+    for suffix_val in Targets.keys():
+        if not Targets[suffix_val]:
+            continue
+        print(suffix_val+":")
+        for suffix_item in Targets[suffix_val].keys():
+            print("\t"+suffix_item+"\t:"+"{:,}".format(Targets[suffix_val][suffix_item]))
+        print('\n')
+
+def ShowSummary(Targets):
+    result = Count(Targets)
+    print("Total:")
+    print("\tFiles\t:"+"{:,}".format(result[0]))
+    print("\tLines\t:"+"{:,}".format(result[1]))
+
+TargetsCount = {}
+Targets = {}
+for val in Suffix:
+    TargetsCount[val] = {}
+    Targets[val] = {}
+
+print('Begin searching files')
+listdir(Path,TargetsCount,True)
+count_result = Count(TargetsCount)
+print('Find ' + str(count_result[0]) + ' files')
+listdir(Path,Targets)
+print('\nResult:')
+ShowResults(Targets)
+ShowSummary(Targets)
